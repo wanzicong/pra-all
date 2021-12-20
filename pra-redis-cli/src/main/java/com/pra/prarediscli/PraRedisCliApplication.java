7d6f42517ab45c14a2403710c19818c617294316
wanzicong <1360816154@qq.com>
2021-12-20 11:14:50 +0800

    添加redis cli

---
+++ pra-redis-cli/src/main/java/com/pra/prarediscli/PraRedisCliApplication.java
@@ -0,0 +1,13 @@
+package com.pra.prarediscli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraRedisCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraRedisCliApplication.class, args);
+    }
+
+}
---
+++ pra-redis-cli/src/test/java/com/pra/prarediscli/PraRedisCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pra.prarediscli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraRedisCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
