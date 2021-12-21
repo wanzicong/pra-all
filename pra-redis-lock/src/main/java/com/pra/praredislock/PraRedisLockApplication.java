e35671d12dd0d4c81e731c7f506795f481aa03db
wanzicong <1360816154@qq.com>
2021-12-21 14:19:46 +0800

    添加redis 锁的功能

---
+++ pra-redis-lock/src/main/java/com/pra/praredislock/PraRedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.pra.praredislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraRedisLockApplication.class, args);
+    }
+
+}
---
+++ pra-redis-lock/src/test/java/com/pra/praredislock/PraRedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pra.praredislock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraRedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
