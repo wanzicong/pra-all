54e50eaf8f50d4a9379182cf2aa7a43edd25bf2d
wanzicong <1360816154@q.com>
2021-12-24 17:14:53 +0800

    添加redisson客户端模块

---
+++ pra-redis-redisson/src/main/java/com/pra/praredisredisson/PraRedisRedissonApplication.java
@@ -0,0 +1,13 @@
+package com.pra.praredisredisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraRedisRedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraRedisRedissonApplication.class, args);
+    }
+
+}
